ede9cae0b677a0fac3dfc2b53d057c00a919ec97
CarolinaOM <caro13081991@gmail.com>
2022-02-01 16:13:46 +0000

    State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld(props){
-  console.log(props)
-  return(
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-    </div>
-  )
-}
+// function Helloworld(props){
+//   console.log(props)
+//   return(
+//     <div id="hello">
+//       <h3>{props.subtitle}</h3>
+//       {props.mytext}
+//     </div>
+//   )
+// }
 
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = ()=> {
+    this.setState({show: !this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return(
+      <div id="hello">
+      <h3>{this.props.subtitle}</h3>
+      {this.props.mytext}
+      <button onClick={this.toggleShow}>Cambiar estado</button>
+      </div>
+      )
+    }else{
+      return <h1>
+        No existen elementos
+        <button onClick={this.toggleShow}>
+          Toggle Show
+        </button>
+      </h1>
+    }
+  }
+}
 function App() {
   return (
     <div>This is my componet: 
